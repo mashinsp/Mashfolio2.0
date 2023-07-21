abc09ca6e25ac856d9710abfb6aa628f143e4958
Syed Mashood <88382605+mashinsp@users.noreply.github.com>
2023-07-21 10:04:18 +0000

    emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -34,8 +34,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        service_j7kyb9z,
+        template_1463f8f,
         {
           from_name: form.name,
           to_name: "Syed Mashood",
@@ -43,7 +43,7 @@ const Contact = () => {
           to_email: "mediafire334@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        -XuLe5HrmNeSI8ufd
       )
       .then(
         () => {
