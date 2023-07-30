e74eea9c2963790e54365f88b1cd0121fcf5e510
Syed Mashood <88382605+mashinsp@users.noreply.github.com>
2023-07-30 05:20:02 +0000

    MEGLB

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -24,9 +24,9 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
+        scale={isMobile ? 0.7 : 4}
         position={isMobile ? [0, -3, -2.2] : [0, -3.6, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        rotation={[0.01, 0, 0]}
       />
     </mesh>
   );
